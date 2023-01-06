ed6c8260688703fe12b7a365b5e39986878228df
justin burnel <justin.burnel@gmail.com>
2023-01-06 23:13:37 +0100

    wesh

--- src/app/pages/characters/characters.module.ts
+++ src/app/pages/characters/characters.module.ts
@@ -2,20 +2,22 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CharactersDumb} from "./dumb/characters.dumb";
 import {CharactersComponentSmart} from "./smart/characters.smart";
-import {NbButtonModule, NbCardModule, NbSelectModule} from "@nebular/theme";
+import {NbButtonModule, NbCardModule, NbInputModule, NbSelectModule} from "@nebular/theme";
 import {RankSelectorComponent} from "./dumb/component/rank-selector/rank-selector.component";
 import {ItemCardComponent} from "./dumb/component/item-card/item-card.component";
 import {FormsModule} from "@angular/forms";
+import {ConverterComponent} from "./dumb/component/converter/converter.component";
 
 @NgModule({
-  declarations: [CharactersComponentSmart, CharactersDumb, RankSelectorComponent, ItemCardComponent],
-    imports: [
-        CommonModule,
-        NbCardModule,
-        NbSelectModule,
-        FormsModule,
-        NbButtonModule,
-    ],
+  declarations: [CharactersComponentSmart, CharactersDumb, RankSelectorComponent, ItemCardComponent,ConverterComponent],
+  imports: [
+    CommonModule,
+    NbCardModule,
+    NbSelectModule,
+    FormsModule,
+    NbButtonModule,
+    NbInputModule,
+  ],
   exports: [CharactersComponentSmart]
 })
 export class CharactersModule {
---
+++ src/app/pages/characters/dumb/component/converter/converter.component.ts
@@ -0,0 +1,15 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-converter-component',
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.scss']
+})
+export class ConverterComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
